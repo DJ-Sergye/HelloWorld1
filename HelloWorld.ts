8e95206ba877cc44bc3455801fdad132745e7f29
Sergio <sergio2498@mail.fresnostate.edu>
2020-04-27 21:42:20 -0700

    added hello world source code

---
+++ HelloWorld.ts
@@ -0,0 +1,15 @@
+
+class HelloWorld {
+
+    constructor(public firstName: string, public lastName: string) {
+
+    }
+
+    sayHello() {
+        console.log('Hello World! ' + 'My name is ' + this.firstName + ' ' + this.lastName + '.')
+    }
+
+}
+
+const myHelloClassInstance = new HelloWorld('Sergio', 'Carrillo');
+myHelloClassInstance.sayHello();
